e7d3e5014b36d7c8321d1ddbb59f3cbc2c86f358
MirManan11 <mirmanan672@gmail.com>
2025-04-22 19:59:51 +0530

    C practice for biggenners

---
+++ C++/demo.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main(){
+    int x;
+    x = 100;
+    int y ;
+    y = 200;
+
+    int z = x + y;
+    printf("Sum of x + y = %d" , z);
+    return 0;
+}
